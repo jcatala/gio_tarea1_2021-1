9f7aa6d824806a5718ad95b5fd369629bf88a612
Dnl-alt-347 <85776389+Dnl-alt-347@users.noreply.github.com>
2021-06-22 03:35:48 -0400

    Update Metodo gradiente ajuste lineal.py

--- Metodo gradiente ajuste lineal.py
+++ Metodo gradiente ajuste lineal.py
@@ -30,13 +30,16 @@ data = {
 def gradiente(pa=0,pb=0,error=0.00001):
 
     #Definición de variables
-    xi,yi,a,b,t = symbols('xi yi a b t',real = True)
+    xi,yi,a,b,t,wi = symbols('xi yi a b t wi',real = True)
     
     #Función objetivo
-    f = (yi - a - (b*xi))**2
+    f = wi * (yi - a - (b*xi))**2
     
     fin = True
     cont=0
+    
+    #Pesos
+    data_wi_g = [0.5989004542062513, 9.473867615628091, 5.082181985659734, 4.204686993718601, 5.35085892855351, 7.9205584660845, 1.6906802612959755, 1.5743640126241567, 50.53483481532306, 1.158923031302284, 1.210955982095339, 2.066633056754798, 1.275232930127079, 1.3603011579583153, 2.813016965621227, 12.78887888913651, 14.937556686238732, 20.348448518917394, 0.7398388302904214, 0.28299968414484483] 
     
     while fin:
 
@@ -48,15 +51,17 @@ def gradiente(pa=0,pb=0,error=0.00001):
         
         #Cálculo de gradiente
         for x1,y1 in data.items():
-            grad_a+= da.evalf(subs={xi:x1,yi:y1,a:pa,b:pb})
-            grad_b+= db.evalf(subs={xi:x1,yi:y1,a:pa,b:pb})
+            
+            grad_a+= da.evalf(subs={xi:x1,yi:y1,a:pa,b:pb,wi:data_wi_g[x1-1]})
+            
+            grad_b+= db.evalf(subs={xi:x1,yi:y1,a:pa,b:pb,wi:data_wi_g[x1-1]})
 
         #Función objetivo en función del tamaño de paso
         g = f.subs([(a,pa-grad_a*t),(b,pb-grad_b*t)])
         sg=0
         
         for x1,y1 in data.items():
-            sg+= g.evalf(subs={xi:x1,yi:y1})
+            sg+= g.evalf(subs={xi:x1,yi:y1,wi:data_wi_g[x1-1]})
         
         dsg = diff(sg,t)
 
@@ -75,8 +80,8 @@ def gradiente(pa=0,pb=0,error=0.00001):
 
         #Se vuelve a calcular el gradiente según la los nuevos valores de a y b
         for x1,y1 in data.items():
-            grad_a+= da.evalf(subs={xi:x1,yi:y1,a:pa,b:pb})
-            grad_b+= db.evalf(subs={xi:x1,yi:y1,a:pa,b:pb})
+            grad_a+= da.evalf(subs={xi:x1,yi:y1,a:pa,b:pb,wi:data_wi_g[x1-1]})
+            grad_b+= db.evalf(subs={xi:x1,yi:y1,a:pa,b:pb,wi:data_wi_g[x1-1]})
             
         #Condición de parada
         if grad_a < error and grad_b < error:
@@ -91,11 +96,6 @@ def gradiente(pa=0,pb=0,error=0.00001):
     print("El número de iteraciones es:",cont)
         
 print("El CPU time es:",timeit.timeit("gradiente()", globals=locals(),number=1),"segundos")
-
-
-
-
-    
 
 
 
